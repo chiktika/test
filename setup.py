54843a4d93a99bb9a030955b8beff6bdb273c075
Chiktika <chiktika@gmail.com>
2020-11-16 10:29:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="test__chiktika", # Replace with your own username
+    name="testchiktika", # Replace with your own username
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
